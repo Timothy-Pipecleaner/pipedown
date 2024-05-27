34728522b28403fd8234b5d41668ca6d9db84c8e
roscomo <chutson99@outlook.com>
2024-05-27 16:53:10 -0500

    Updates to the way pipes work

--- nodes/area_pipe.gd
+++ nodes/area_pipe.gd
@@ -1,18 +1,33 @@
+@tool
 extends Area2D
 class_name AreaPipe
 
 @export var pipe_name: String
+@export var spawn_point: Vector2:
+	set(value):
+		if(Engine.is_editor_hint()):
+			queue_redraw()
+		spawn_point = value
+
 var has_stepped_away = true
 signal pipe_entered(pipe_name: String)
 
 func _enter_tree() -> void:
+	if(Engine.is_editor_hint()): return
 	body_entered.connect(on_body_entered)
 	body_exited.connect(on_body_exited)
 
 func on_body_entered(node: Node2D) -> void:
+	if(Engine.is_editor_hint()): return
 	if has_stepped_away:
 		pipe_entered.emit(pipe_name)
 
 func on_body_exited(node: Node2D) -> void:
+	if(Engine.is_editor_hint()): return
 	if !has_stepped_away:
 		has_stepped_away = true
+
+func _draw():
+	if(!Engine.is_editor_hint()): return
+	draw_circle(spawn_point, 13, Color.WHITE)	
+	draw_circle(spawn_point, 12, Color(Color.BLUE_VIOLET, 0.75))
--- nodes/room.gd
+++ nodes/room.gd
@@ -7,16 +7,16 @@ var transition_ready = false
 
 signal room_exited(room_name: String, pipe_name: String)
 
-func _ready():
-	for child in get_children():
-		if child.has_signal("pipe_entered"):
-			child.connect("pipe_entered", on_pipe_entered)
-			pipes[child.pipe_name] = child
+func _notification(what):
+	if what == NOTIFICATION_SCENE_INSTANTIATED:
+		for child in get_children():
+			if child.has_signal("pipe_entered"):
+				child.connect("pipe_entered", on_pipe_entered)
+				pipes[child.pipe_name] = child
 
 func on_pipe_entered(pipe_name: String):
 	room_exited.emit(room_name, pipe_name)
 
 func place(node: Node2D, pipe_name: String):
-	pipes[pipe_name].has_stepped_away = false
-	node.position = pipes[pipe_name].position
-	
+	node.position = to_local(pipes[pipe_name].to_global(pipes[pipe_name].spawn_point))
+	add_child(node)
